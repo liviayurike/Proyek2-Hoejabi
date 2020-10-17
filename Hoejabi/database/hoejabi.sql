052e1201065bdbdb1175ec680ca86231cdd04223
unknown <dimahyantidwi@gmail.com>
2020-10-17 16:54:34 +0700

    Transaksi

--- Hoejabi/database/hoejabi.sql
+++ Hoejabi/database/hoejabi.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 17 Okt 2020 pada 03.26
--- Versi Server: 10.1.10-MariaDB
+-- Generation Time: Oct 17, 2020 at 11:53 AM
+-- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,7 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `Nama` varchar(30) NOT NULL,
+  `No Telp/Wa` varchar(30) NOT NULL,
+  `Alamat` varchar(50) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Product` varchar(50) NOT NULL,
+  `Harga` decimal(11,0) NOT NULL,
+  `Total Harga` decimal(10,0) NOT NULL,
+  `Via Pembayaran` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`Nama`, `No Telp/Wa`, `Alamat`, `Email`, `Product`, `Harga`, `Total Harga`, `Via Pembayaran`) VALUES
+('nining', '0816893754', 'btn nyiur permai', 'nining@gmail.com', 'inner', '22', '22', 'atm'),
+('nining', '0816893754', 'btn nyiur permai', 'nining@gmail.com', 'inner', '22000', '22000', 'atm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -34,7 +61,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `nama`) VALUES
@@ -61,6 +88,8 @@ ALTER TABLE `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
